43d5ffc2ba741f7f0ef33a5f244b50c6da89fcce
Ruben <ruben.birsan1@gmail.com>
2019-02-26 22:47:53 +0200

    Files directory added

--- ProductAPI/Controllers/ProductController.cs
+++ ProductAPI/Controllers/ProductController.cs
@@ -14,13 +14,12 @@ namespace ProductAPI.Controllers {
 
         [HttpGet("GetAllProducts")]
         public ActionResult<IEnumerable<Product>> GetAllProducts() {
-           return JsonConvert.DeserializeObject<List<Product>>(System.IO.File.ReadAllText("bin/products.json"));   
+           return JsonConvert.DeserializeObject<List<Product>>(System.IO.File.ReadAllText("Files/products.json"));   
         }
 
         [HttpGet("GetProductById/{id}")]
         public ActionResult<Product> GetProductById(int id) {
-           return JsonConvert.DeserializeObject<IEnumerable<Product>>(System.IO.File.ReadAllText("bin/products.json")).FirstOrDefault(w=>w.ProductId == id);
+           return JsonConvert.DeserializeObject<IEnumerable<Product>>(System.IO.File.ReadAllText("Files/products.json")).FirstOrDefault(w=>w.ProductId == id);
         }
-
     }
 }
